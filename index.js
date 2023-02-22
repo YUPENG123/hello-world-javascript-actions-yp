19f12c7722ccc886ce108f8f2f69ced3c2beff8c
YUPENG123 <yupengdongguan@hotmail.com>
2023-02-22 21:33:17 +0800

    credit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const github = require("@actions/github");
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
+  console.log("Hello ${nameToGreet}!");
   //var envMy = process.env;
   //console.log(envMy);
   console.log("HELLO WORLD!!!");
@@ -12,7 +12,7 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  console.log("The event payload: ${payload}");
 } catch (error) {
   core.setFailed(error.message);
 }
