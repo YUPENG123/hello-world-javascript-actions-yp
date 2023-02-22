357eb50d5eba885439ba9d39d3d25fa2203f1072
YUPENG123 <yupengdongguan@hotmail.com>
2023-02-22 21:16:30 +0800

    cffg

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
-  var env=process.env();
-  console.log(env);
-  const time = (new Date()).toTimeString();
+  var envMy = process.env;
+  console.log(envMy);
+  const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
