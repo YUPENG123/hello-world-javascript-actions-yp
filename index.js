017464f1fced996dcd61afb0d0d60476dcce295d
YUPENG123 <yupengdongguan@hotmail.com>
2023-02-22 19:48:34 +0800

    funny

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+  var env=process.env();
+  console.log(env);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
