3a4d5fedbb6aec27193a3c648365de579680bd21
yp <yp523ne@163.com>
2023-03-08 20:02:42 +0800

    adasd

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+  core.debug('Inside try block');
 } catch (error) {
   core.setFailed(error.message);
 }
